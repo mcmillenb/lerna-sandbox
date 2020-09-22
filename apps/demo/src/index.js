9305836c17a5531dd6f318f801ecd4056e8e0b0a
Brian McMillen <brianmcmillen1@gmail.com>
2020-09-21 20:34:19 -0400

    update the demo app to see what happens

--- apps/demo/src/index.js
+++ apps/demo/src/index.js
@@ -3,7 +3,8 @@ import and from '@thanksd/and-function';
 const a = true;
 const b = 'brian';
 const c = 1000;
+const d = {};
 
-if (and(a, b, c)) {
+if (and(a, b, c, d)) {
   require('@thanksd/say-hello');
 }
